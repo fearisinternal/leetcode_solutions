8f221b07ce17e41b3faf98d13ccab1f751491956
fearisinternal <nadenisova98@gmail.com>
2023-12-10 17:22:22 +0400

    Fix solved tasks: 1716, 867

--- 1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank.cpp
@@ -1,23 +1,17 @@
-#include <iostream>
-#include <algorithm>
-
-int totalMoney(int n)
-{
-    int sum = 0;
-    int start = 1;
-    int days = 7;
-    while (n > 0)
-    {
-        if (n < 7)
-            days = n;
-        sum += (days * (start + start + days - 1)) / 2;
-        n -= days;
-        start++;
+class Solution {
+public:
+    int totalMoney(int n) {
+        int sum = 0;
+        int start = 1;
+        int days = 7;
+        while (n > 0)
+        {
+            if (n < 7)
+                days = n;
+            sum += (days * (start + start + days - 1)) / 2;
+            n -= days;
+            start++;
+        }
+        return sum;
     }
-    return sum;
-}
-
-int main()
-{
-    std::cout << totalMoney(20);
-}
+};
--- 867-transpose-matrix.cpp
+++ 867-transpose-matrix.cpp
@@ -1,20 +1,14 @@
-#include <iostream>
-#include <vector>
-
-std::vector<std::vector<int>> transpose(std::vector<std::vector<int>> &matrix)
-{
-    std::vector<std::vector<int>> invert(matrix[0].size(), std::vector<int>(matrix.size()));
-    for (auto i = 0; i < matrix.size(); i++)
-    {
-        for (auto j = 0; j < matrix[i].size(); j++)
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        std::vector<std::vector<int>> invert(matrix[0].size(), std::vector<int>(matrix.size()));
+        for (auto i = 0; i < matrix.size(); i++)
         {
-            invert[j][i] = matrix[i][j];
+            for (auto j = 0; j < matrix[i].size(); j++)
+            {
+                invert[j][i] = matrix[i][j];
+            }
         }
+        return invert;
     }
-    return invert;
-}
-
-int main()
-{
-    return 0;
-}
+};
